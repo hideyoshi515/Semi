9507d0bcaac61ed44dfcc47dd87d1a529c988bb9
MinSeunghyun <nadamdh1@naver.com>
2024-06-03 15:23:57 +0900

    [add] commit MSH codes at 24-06-03 PM15:23

--- src/main/java/co/kr/necohost/semi/domain/service/SalesService.java
+++ src/main/java/co/kr/necohost/semi/domain/service/SalesService.java
@@ -12,21 +12,30 @@ public class SalesService {
     private final SalesRepository salesRepository;
     SalesRepository SalesRepository;
 
-    public SalesService(SalesRepository salesRepository, SalesRepository salesRepository) {
+    public SalesService(SalesRepository salesRepository) {
         this.SalesRepository = salesRepository;
         this.salesRepository = salesRepository;
     }
     //create
-    public void save(SalesRequest)
+    public void save(SalesRequest salesRequest) {
+        salesRepository.save(salesRequest.toEntity());
+    }
 
 
     //read everything
     public List<Sales> findAll() {
         return salesRepository.findAll();
     }
-    //read by id
+    //read by id//update?
     public Sales findById(Long id) {
         return salesRepository.findById(id).orElse(null);
     }
+
+    //delete
+    public void deleteById(SalesRequest salesRequest) {
+        salesRepository.deleteById(salesRequest.getId());
+    }
+
+
 
 }
