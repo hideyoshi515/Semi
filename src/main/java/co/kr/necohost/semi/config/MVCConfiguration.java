e164734c744768d7f87f4ee0542fdc7b63777738
hideyoshi515 <hideyoshi515@gmail.com>
2024-06-04 10:57:45 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/co/kr/necohost/semi/config/MVCConfiguration.java
+++ src/main/java/co/kr/necohost/semi/config/MVCConfiguration.java
@@ -1,15 +1,35 @@
 package co.kr.necohost.semi.config;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@EnableCaching
 public class MVCConfiguration implements WebMvcConfigurer {
+
+    @GetMapping("/redirected")
+    public String redirected() {
+        return "redirected";
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600);
+    }
+
+    @Bean
+    public ConcurrentMapCacheManager cacheManager(){
+        return new ConcurrentMapCacheManager(
+                "categories"
+        );
     }
 }
