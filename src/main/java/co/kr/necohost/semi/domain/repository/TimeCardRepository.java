ea83ef8740d5e5af51fc02cda3e70178c356c7e3
lsy <curio1032@gmail.com>
2024-06-13 18:23:56 +0900

    ready for function staff-timeCard system

---
+++ src/main/java/co/kr/necohost/semi/domain/repository/TimeCardRepository.java
@@ -0,0 +1,11 @@
+package co.kr.necohost.semi.domain.repository;
+
+import co.kr.necohost.semi.domain.model.entity.Staff;
+import co.kr.necohost.semi.domain.model.entity.TimeCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TimeCardRepository extends JpaRepository<TimeCard, Long> {
+    Optional<TimeCard> findTopByStaffOrderByStartDesc(Staff staff);
+}
