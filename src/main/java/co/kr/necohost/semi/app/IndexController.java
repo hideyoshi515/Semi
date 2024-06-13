3b8da9e2c22e3ccd861726a2e5fbbe9cb4206c03
hideyoshi515 <hideyoshi515@gmail.com>
2024-06-13 10:37:07 +0900

    Complete Kiosk Service

--- src/main/java/co/kr/necohost/semi/app/IndexController.java
+++ src/main/java/co/kr/necohost/semi/app/IndexController.java
@@ -1,12 +1,10 @@
 package co.kr.necohost.semi.app;
 
-import co.kr.necohost.semi.domain.service.CategoryService;
 import co.kr.necohost.semi.domain.service.SalesService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -14,22 +12,19 @@ import java.util.Map;
 @Controller
 public class IndexController {
 
-    SalesService salesService;
-    CategoryService categoryService;
+    private final SalesService salesService;
 
-    public IndexController(SalesService salesService, CategoryService categoryService) {
-        this.categoryService = categoryService;
+    public IndexController(SalesService salesService) {
         this.salesService = salesService;
     }
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    @GetMapping("/index")
     public String getIndex(Model model, @RequestParam(name = "lang", required = false) String lang, HttpSession session) {
         model.addAttribute("session", session);
-        model.addAttribute("categories", categoryService.getAllCategories());
-        return "user/home.html";
+        return "redirect:/";
     }
 
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    @GetMapping("/admin")
     public String getAdmin(Model model, @RequestParam(name = "lang", required = false) String lang, HttpSession session) {
         Map<String, Long> todaySales = salesService.findSalesByToday();
         model.addAttribute("todaySales", todaySales);
--- src/main/java/co/kr/necohost/semi/app/login/LoginController.java
+++ src/main/java/co/kr/necohost/semi/app/login/LoginController.java
@@ -4,137 +4,104 @@ import co.kr.necohost.semi.domain.model.dto.AccountRequest;
 import co.kr.necohost.semi.domain.model.entity.Account;
 import co.kr.necohost.semi.domain.repository.AccountRepository;
 import co.kr.necohost.semi.domain.service.AccountService;
-import co.kr.necohost.semi.domain.service.CategoryService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
 public class LoginController {
     private final AccountRepository accountRepository;
-    CategoryService categoryService;
-    AccountService accountService;
+    private final AccountService accountService;
 
-    public LoginController(CategoryService categoryService, AccountService accountService, AccountRepository accountRepository) {
-        this.categoryService = categoryService;
+    public LoginController(AccountService accountService, AccountRepository accountRepository) {
         this.accountService = accountService;
         this.accountRepository = accountRepository;
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public String getRegister(Model model, HttpSession session, HttpServletRequest request, @RequestParam Map<String, Object> params) {
-        AccountRequest accountRequest = new AccountRequest();
-        model.addAttribute("session", session);
-        model.addAttribute("categories", categoryService.getAllCategories());
-        String uri = request.getHeader("Referer");
-        if (uri != null && !uri.contains("/register") && !uri.contains("/login")) {
-            session.setAttribute("prevpage", uri);
-        } else {
-            session.setAttribute("prevpage", "/");
-        }
+    @GetMapping("/register")
+    public String getRegister(Model model, HttpSession session, HttpServletRequest request) {
+        setPreviousPage(session, request, "/register");
         if (session.getAttribute("account") != null) {
             return "redirect:" + session.getAttribute("prevpage");
         }
-        if (session.getAttribute("accountRequest") != null) {
-            accountRequest = (AccountRequest) session.getAttribute("accountRequest");
+        AccountRequest accountRequest = (AccountRequest) session.getAttribute("accountRequest");
+        if (accountRequest == null) {
+            accountRequest = new AccountRequest();
         }
+        model.addAttribute("session", session);
         model.addAttribute("accountRequest", accountRequest);
         return "login/register.html";
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String postRegister(Model model, @Valid @ModelAttribute("accountRequest") AccountRequest accountRequest, Errors errors, HttpSession session, @RequestParam Map<String, Object> params) {
-        String canCreate = "";
+    @PostMapping("/register")
+    public String postRegister(Model model, @Valid @ModelAttribute("accountRequest") AccountRequest accountRequest, Errors errors, HttpSession session) {
         if (errors.hasErrors()) {
             model.addAttribute("accountRequest", accountRequest);
             Map<String, String> validatorResult = accountService.validateHandling(errors);
-            for (String error : validatorResult.keySet()) {
-                model.addAttribute(error, validatorResult.get(error));
-            }
+            validatorResult.forEach(model::addAttribute);
             return "login/register.html";
         }
-        boolean errored = false;
+
         if (accountService.isExit(accountRequest)) {
-            canCreate = "既に存在するメールアドレスです";
-            model.addAttribute("valid_email", canCreate);
-            errored = true;
-        }
-        if (accountService.isPhoneExit(accountRequest)) {
-            canCreate = "既に存在する携帯電話番号です";
-            model.addAttribute("valid_phone", canCreate);
-            errored = true;
-        }
-        if (errored) {
+            model.addAttribute("valid_email", "既に存在するメールアドレスです");
             return "login/register.html";
         }
+
+        if (accountService.isPhoneExit(accountRequest)) {
+            model.addAttribute("valid_phone", "既に存在する携帯電話番号です");
+            return "login/register.html";
+        }
+
         accountService.save(accountRequest);
         Account account = accountRepository.findByEmail(accountRequest.getEmail()).orElse(null);
         session.setAttribute("account", account);
-        session.setMaxInactiveInterval(60 * 60 * 24); //60s * 60m * 24h
+        session.setMaxInactiveInterval(60 * 60 * 24); // 24 hours
         session.removeAttribute("accountRequest");
         return "redirect:" + session.getAttribute("prevpage");
-
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String getLogin(Model model, HttpSession session, HttpServletRequest request, @RequestParam Map<String, Object> params) {
-        String uri = request.getHeader("Referer");
-        if (uri != null && !uri.contains("/register") && !uri.contains("/login")) {
-            session.setAttribute("prevpage", uri);
-        } else {
-            session.setAttribute("prevpage", "/");
-        }
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpSession session, HttpServletRequest request) {
+        setPreviousPage(session, request, "/login");
         AccountRequest accountRequest = new AccountRequest();
         model.addAttribute("session", session);
-        model.addAttribute("categories", categoryService.getAllCategories());
         model.addAttribute("accountRequest", accountRequest);
         return "login/login.html";
     }
 
-    @RequestMapping(value = "/mypage", method = RequestMethod.GET)
-    public String getMypage(Model model, HttpSession session, @RequestParam Map<String, Object> params) {
+    @GetMapping("/mypage")
+    public String getMypage(Model model, HttpSession session) {
         Account account = (Account) session.getAttribute("account");
         if (account == null) {
-            model.addAttribute("account", null);
             return "redirect:/login";
-        } else {
-            model.addAttribute("account", account);
         }
-
-        AccountRequest accountRequest = account.toAccountRequest();
-
-        model.addAttribute("accountRequest", accountRequest);
+        model.addAttribute("account", account);
+        model.addAttribute("accountRequest", account.toAccountRequest());
         model.addAttribute("session", session);
-        model.addAttribute("categories", categoryService.getAllCategories());
         return "user/mypage.html";
     }
 
-    @RequestMapping(value = "/mypage", method = RequestMethod.POST)
-    public String postMypage(Model model, HttpSession session, @ModelAttribute("accountRequest") AccountRequest accountRequest, @RequestParam Map<String, Object> params) {
+    @PostMapping("/mypage")
+    public String postMypage(HttpSession session, @ModelAttribute("accountRequest") AccountRequest accountRequest, @RequestParam Map<String, Object> params) {
         Account account = (Account) session.getAttribute("account");
         AccountRequest newRequest = account.toAccountRequest();
         boolean changed = false;
-        System.out.println("passEdit"+params.get("passEdit"));
-        System.out.println("phoneEdit"+params.get("phoneEdit"));
-        System.out.println("msPassEdit"+params.get("msPassEdit"));
-        if (params.get("passEdit") != null && params.get("passEdit").toString().equals("edit")) {
+
+        if ("edit".equals(params.get("passEdit"))) {
             newRequest.setPassword(accountRequest.getPassword());
             changed = true;
         }
-        if (params.get("phoneEdit") != null && params.get("phoneEdit").toString().equals("edit")) {
+        if ("edit".equals(params.get("phoneEdit"))) {
             newRequest.setPhone(accountRequest.getPhone());
             changed = true;
         }
-        if (params.get("msPassEdit") != null && params.get("msPassEdit").toString().equals("edit")) {
+        if ("edit".equals(params.get("msPassEdit"))) {
             newRequest.setMsPass(accountRequest.getMsPass());
             changed = true;
         }
@@ -143,5 +110,14 @@ public class LoginController {
             session.setAttribute("account", newRequest.toEntity());
         }
         return "redirect:/";
+    }
+
+    private void setPreviousPage(HttpSession session, HttpServletRequest request, String currentPath) {
+        String uri = request.getHeader("Referer");
+        if (uri != null && !uri.contains(currentPath) && !uri.contains("/login")) {
+            session.setAttribute("prevpage", uri);
+        } else {
+            session.setAttribute("prevpage", "/");
+        }
     }
 }
