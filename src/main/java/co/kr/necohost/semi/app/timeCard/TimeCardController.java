24526b9077dbce8b22dcfdad819d99640c55800d
lsy <curio1032@gmail.com>
2024-06-14 18:20:46 +0900

    fix timeCardIn&Out

--- src/main/java/co/kr/necohost/semi/app/timeCard/TimeCardController.java
+++ src/main/java/co/kr/necohost/semi/app/timeCard/TimeCardController.java
@@ -45,7 +45,7 @@ public class TimeCardController {
         try {
             timeCardService.clockIn(request);
             Optional<TimeCard> timeCard = timeCardService.getTimeCardByUserName(request);
-            discordBotService.sendOrderNotification(request.getUsername() + "이/가 " + "" + "에 출근했습니다.");
+            discordBotService.sendOrderNotification(request.getUsername() + "이/가 " + timeCard.get().getStart() + "에 출근했습니다.");
             return ResponseEntity.ok("Clock in attempted.");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
@@ -56,7 +56,8 @@ public class TimeCardController {
     public ResponseEntity<String> postTimeCardOut(@RequestBody StaffRequest request) {
         try {
             timeCardService.clockOut(request);
-            discordBotService.sendOrderNotification(request.getUsername() + "이/가" + "" + "에 퇴근했습니다.");
+            Optional<TimeCard> timeCard = timeCardService.getTimeCardByUserName(request);
+            discordBotService.sendOrderNotification(request.getUsername() + "이/가 " + timeCard.get().getEnd() + "에 퇴근했습니다.");
             return ResponseEntity.ok("Clock out attempted.");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
