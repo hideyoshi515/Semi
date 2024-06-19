e0d9f9743d448ffa767e2bd06b26e7be7d755fc7
hideyoshi515 <hideyoshi515@gmail.com>
2024-06-19 17:49:57 +0900

    LSH Clear the ToDo

--- src/main/java/co/kr/necohost/semi/app/user/UserController.java
+++ src/main/java/co/kr/necohost/semi/app/user/UserController.java
@@ -1,25 +1,35 @@
 package co.kr.necohost.semi.app.user;
 
 import co.kr.necohost.semi.domain.model.entity.Account;
+import co.kr.necohost.semi.domain.model.entity.Menu;
 import co.kr.necohost.semi.domain.repository.AccountRepository;
+import co.kr.necohost.semi.domain.service.MenuService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @Controller
 public class UserController {
     private final AccountRepository accountRepository;
+    private final MenuService menuService;
 
-    public UserController(AccountRepository accountRepository) {
+    public UserController(AccountRepository accountRepository, MenuService menuService) {
         this.accountRepository = accountRepository;
+        this.menuService = menuService;
     }
 
     // ホームページのリクエストを処理するメソッド
     @RequestMapping("/")
     public String index(Model model, HttpSession session) {
+        List<Menu> menuList = menuService.getAllMenus();
+        Random rand = new Random();
+        model.addAttribute("menuList", menuList);
+        model.addAttribute("randomMenu", menuList.get(rand.nextInt(menuList.size())));
         model.addAttribute("session", session);
         return "user/home.html";
     }
